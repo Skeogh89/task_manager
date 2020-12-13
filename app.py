0f14e9ce3b759f95b030b232511414f39f5aaa0e
Stephen Keogh <stevokeogh@hotmail.com>
2020-12-13 19:26:35 +0000

    added styles and JS

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import (Flask, flash, render_template, 
-                    redirect, request, session, 
-                    url_for)
-
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
+
 
 app = Flask(__name__)
 
@@ -16,17 +16,15 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_tasks")
-
-
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
 
-
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
